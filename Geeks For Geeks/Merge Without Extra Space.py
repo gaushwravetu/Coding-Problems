5d3a0a099e12457e40faf6a79f1c460a67ca0774
gauravappy97 <gauravappy1997@gmail.com>
2020-07-12 12:27:30 +0530

    gfg updated

---
+++ Geeks For Geeks/Merge Without Extra Space.py
@@ -0,0 +1,10 @@
+    n, m = list(map(int,input().split()))
+    l1 = list(map(int,input().split()))
+    l2 = list(map(int,input().split()))
+    for i in range(len(l2)):
+        l1.append(l2[i])
+    l1.sort()
+    for x in l1:
+        print(x,end = " ")
+    print()
+        
