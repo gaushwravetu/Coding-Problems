4537fe4f1b13741d4d0db82ac0abc8c14740138e
gauravappy97 <gauravappy1997@gmail.com>
2020-07-25 00:25:04 +0530

    hackerRank updated

--- HackerRank/Problem Solving/count the leaderboard.py
+++ HackerRank/Problem Solving/count the leaderboard.py
@@ -5,44 +5,19 @@ import os
 import random
 import re
 import sys
-def binsearch(alice,result):
-    l = 0
-    h = len(result)-1
-    while(l<=h):
-        mid = l+((h-l)//2)
-        if(mid==alice):
-            return mid
-        elif(result[mid]<alice and alice<result[mid-1]):
-            return mid
-        elif(result[mid]>alice and alice>=result[mid+1]):
-            return mid+1
-        elif(result[mid]<alice):
-            h = mid-1
-        elif(result[mid]>alice):
-            l = mid+1
-    return -1
 # Complete the climbingLeaderboard function below.
 def climbingLeaderboard(scores, alice, scores_count, alice_count):
-    result = [0]*(scores_count)
-    result[0]=1
-    res = [0]*(alice_count)
-    for i in range(1,scores_count):
-        if(scores[i]==scores[i-1]):
-            result[i]=result[i-1]
-        else:
-            result[i] = result[i-1]+1
-    print(result)
+    scores = sorted(list(set(scores)),reverse=True)
+    alice.sort(reverse=True)
+    result = []*alice_count
+    j = 0
+    scores_count = len(scores)
     for i in range(alice_count):
-        if alice[i]>scores[0]:
-            res[i] = 1
-        elif alice[i]<scores[scores_count-1]:
-            res[i] = result[scores_count-1]+1
-        else:
-            index = binsearch(alice[i],scores)
-            res[i] = result[index]
-            #print(index)
-    #print(res)
-    return res    
+        while j<scores_count and alice[i]<scores[j]:
+            j+=1
+        result.append(j+1)
+    result.sort(reverse=True)
+    return result
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
